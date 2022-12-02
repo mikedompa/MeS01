c14926b024640f831907b288d6e82e744214b958
miguel <miguel.dedompablomusulen@gmail.com>
2022-12-02 12:11:01 +0100

    commiiiit mes

--- MaquinaExpendedoraSimple.java
+++ MaquinaExpendedoraSimple.java
@@ -16,7 +16,7 @@
      * precio del billete y el origen y destino dados. Se asume que el precio
      * del billete que se recibe es mayor que 0.
      */
-    public MaquinaExpendedoraSimple(int precioDelBillete, String origen, String destino) {
+    public MaquinaExpendedoraSimple {
         precioBillete = 20;
         balanceClienteActual = 0;
         totalDineroAcumulado = 0;
