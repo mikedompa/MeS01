6983cb4aa5ad325107d853a9195396e8d5b58e56
miguel <miguel.dedompablomusulen@gmail.com>
2022-12-12 09:26:31 +0100

    mes commit

--- MaquinaExpendedoraSimple.java
+++ MaquinaExpendedoraSimple.java
@@ -34,8 +34,8 @@
     /**
      * Devuelve el precio del billete
      */
-    public void precioBillete(int precioReduxBillete) {
-        precioBillete = precioBillete - precioReduxBillete;
+    public void precioBillete(int precioDelBillete) {
+        precioBillete = precioBillete - precioDelBillete;
     }
 
     /**
